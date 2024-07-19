84e2145a0b17e4d8f199c2dbbfddc16100911399
Gear Shift <gearshift@gearshiftgaming.com>
2024-07-18 22:40:12 -0400

    Created initial main view for UI in JavaFX

--- src/main/java/com/gearshiftgaming/se_mod_manager/SpaceEngineersModManager.java
+++ src/main/java/com/gearshiftgaming/se_mod_manager/SpaceEngineersModManager.java
@@ -7,6 +7,13 @@ import com.gearshiftgaming.se_mod_manager.backend.domain.ModService;
 import com.gearshiftgaming.se_mod_manager.backend.domain.SandboxService;
 import com.gearshiftgaming.se_mod_manager.backend.models.Mod;
 import com.gearshiftgaming.se_mod_manager.frontend.ui.ModManagerView;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,13 +25,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-public class SpaceEngineersModManager {
+public class SpaceEngineersModManager extends Application {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
 
         final Logger logger = LogManager.getLogger(SpaceEngineersModManager.class);
         logger.info("Started application...");
-
+        launch();
+/*
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException |
@@ -51,6 +59,27 @@ public class SpaceEngineersModManager {
         //Get the party started
         modManagerController.injectModList();
         logger.info("Application finished. Closing.");
+*/
+    }
 
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+
+        //TODO: Set a context menu on the menu header ONLY per this https://stackoverflow.com/questions/47786125/how-to-add-a-context-menu-to-an-empty-tableviews-header-row. This may however, not be a good idea. Check the notes for the todo. Fourth item, sub-item of item 3. Also use this to inject checkboxes!
     }
 }
