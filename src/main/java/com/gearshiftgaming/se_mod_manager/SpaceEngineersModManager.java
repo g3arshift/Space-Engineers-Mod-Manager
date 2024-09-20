43c4b64628c4a27279013cdfb3e76c41883e450a
Gear Shift <gearshift@gearshiftgaming.com>
2024-09-20 15:49:02 -0400

    Fixed exception logging

--- src/main/java/com/gearshiftgaming/se_mod_manager/SpaceEngineersModManager.java
+++ src/main/java/com/gearshiftgaming/se_mod_manager/SpaceEngineersModManager.java
@@ -37,19 +37,24 @@ public class SpaceEngineersModManager extends Application {
 	@Override
 	public void start(Stage primaryStage) throws IOException, XmlPullParserException, JAXBException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
 		new ViewController(primaryStage, logger);
-		Thread.setDefaultUncaughtExceptionHandler(SpaceEngineersModManager::logError);
+		Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
+			try {
+				logError(t, e);
+			} catch (Throwable ex) {
+				Platform.exit();
+			}
+		});
 		primaryStage.show();
 	}
 
 	//Log the error that caused our stacktrace to the log, and shutdown the application.
-	private static void logError(Thread t, Throwable e){
+	private static void logError(Thread t, Throwable e) throws Throwable {
 		//Second condition is a dirty hack to prevent it from double displaying errors when we close the platform
-		if (Platform.isFxApplicationThread() && !e.getLocalizedMessage().contains("Key not associated with a running event loop")) {
-			logger.error(e.getLocalizedMessage());
-			logger.error(Arrays.toString(e.getStackTrace()));
+		if (Platform.isFxApplicationThread()) {
+			logger.error("Uncaught exception in thread: " + t.getName(), e);
 			Popup.displaySimpleAlert("An unexpected error was encountered and the application will now exit. " +
 					"Please submit a bug report along with your SEMM.log file located in the logs folder to the below link.", "https://spaceengineersmodmanager.com/bugreport", MessageType.ERROR);
-			Platform.exit();
+			throw e;
 		}
 	}
 }
