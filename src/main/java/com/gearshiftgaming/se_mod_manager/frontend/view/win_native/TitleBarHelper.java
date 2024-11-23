2f1b6e5cf5b7971efecca05d5e9f10b312a43012
Gear Shift <gearshift@gearshiftgaming.com>
2024-11-22 22:45:52 -0500

    Continued work on native titlebar modification

---
+++ src/main/java/com/gearshiftgaming/se_mod_manager/frontend/view/win_native/TitleBarHelper.java
@@ -0,0 +1,23 @@
+package com.gearshiftgaming.se_mod_manager.frontend.view.win_native;
+
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.WinDef;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import lombok.val;
+
+/**
+ * Copyright (C) 2024 Gear Shift Gaming - All Rights Reserved
+ * You may use, distribute, and modify this code under the terms of the GPL3 license.
+ * <p>
+ * You should have received a copy of the GPL3 license with
+ * this file. If not, please write to: gearshift@gearshiftgaming.com.
+ */
+
+//TODO: Class is junk, read this https://medium.com/swlh/customizing-the-title-bar-of-an-application-window-50a4ac3ed27e
+//TODO: Original SO source https://stackoverflow.com/questions/42583779/how-to-change-the-color-of-title-bar-in-framework-javafx#42584185
+
+public class TitleBarHelper {
+
+}
