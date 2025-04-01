868483896f7cc478b0770b980209b10d19bcfef6
g3arshift <djnightmarenight@gmail.com>
2025-04-01 01:36:10 -0400

    Replaced fromSTring function for SEVersion class with valueOf

--- src/main/java/com/gearshiftgaming/se_mod_manager/backend/models/SpaceEngineersVersion.java
+++ src/main/java/com/gearshiftgaming/se_mod_manager/backend/models/SpaceEngineersVersion.java
@@ -1,19 +1,6 @@
 package com.gearshiftgaming.se_mod_manager.backend.models;
 
 public enum SpaceEngineersVersion {
-    SPACE_ENGINEERS_ONE("SPACE_ENGINEERS_ONE"),
-    SPACE_ENGINEERS_TWO("SPACE_ENGINEERS_TWO");
-
-    private final String name;
-
-    SpaceEngineersVersion(String name) {this.name = name;}
-
-    public static SpaceEngineersVersion fromString(String name) {
-        for(SpaceEngineersVersion b : SpaceEngineersVersion.values()) {
-            if(b.name.equalsIgnoreCase((name))) {
-                return b;
-            }
-        }
-        return null;
-    }
+    SPACE_ENGINEERS_ONE,
+    SPACE_ENGINEERS_TWO;
 }
