07799411eb23b95c680a508e7f499efab2964534
g3arshift <djnightmarenight@gmail.com>
2025-07-08 22:25:50 -0400

    Fixed bug with filter check in OperatingSystemVersionUtility

--- src/main/java/com/gearshiftgaming/se_mod_manager/OperatingSystemVersionUtility.java
+++ src/main/java/com/gearshiftgaming/se_mod_manager/OperatingSystemVersionUtility.java
@@ -11,6 +11,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
+ * Utility class for determining the operating system version.
+ * <p>
+ * This class attempts to identify whether the host OS is Linux or a supported version of Windows
+ * (Windows 10 or 11). It uses the {@link CommandRunner} to execute version-checking commands on Windows
+ * and parses the build number to distinguish between versions.
+ * <p>
  * Copyright (C) 2025 Gear Shift Gaming - All Rights Reserved
  * You may use, distribute, and modify this code under the terms of the GPL3 license.
  * <p>
@@ -24,6 +30,14 @@ public class OperatingSystemVersionUtility {
     private OperatingSystemVersionUtility() {
     }
 
+    /**
+     * Determines the current operating system version.
+     *
+     * @return the {@link OperatingSystemVersion} enum value representing the detected OS.
+     * @throws IOException                     if the version check command fails to run.
+     * @throws InterruptedException            if the command execution is interrupted.
+     * @throws UnknownOperatingSystemException if the OS cannot be determined.
+     */
     public static OperatingSystemVersion getOperatingSystemVersion() throws IOException, InterruptedException {
         if (isLinux())
             return OperatingSystemVersion.LINUX;
@@ -33,6 +47,16 @@ public class OperatingSystemVersionUtility {
             throw new UnknownOperatingSystemException("The operating system is unable to be determined");
     }
 
+    /**
+     * Uses the `cmd /c ver` command to determine whether the Windows version is 10 or 11.
+     * Parses the build number from the command output and maps it to a known version.
+     *
+     * @return the {@link OperatingSystemVersion} for the current Windows system.
+     * @throws IOException                     if the command fails to run.
+     * @throws InterruptedException            if the command is interrupted.
+     * @throws CommandRunnerException          if the command exits unsuccessfully or output is unparseable.
+     * @throws UnknownOperatingSystemException if the build number is outside known version ranges.
+     */
     private static OperatingSystemVersion getWindowsVersion() throws IOException, InterruptedException {
         CommandResult commandResult = COMMAND_RUNNER.runCommand(List.of("cmd.exe", "/c", "ver"));
         if (!commandResult.wasSuccessful())
@@ -44,19 +68,24 @@ public class OperatingSystemVersionUtility {
                     Matcher m = Pattern.compile("Version \\d+\\.\\d+\\.(\\d+)").matcher(line);
                     return m.find() ? Integer.parseInt(m.group(1)) : -1;
                 })
-                .filter(buildNumber -> buildNumber != -1 && buildNumber <= 22000)
+                .filter(buildNumber -> buildNumber != -1)
                 .findFirst()
                 .map(buildNumber -> {
-                    if(buildNumber >= 22000) //Earliest Windows 11 build
+                    if (buildNumber >= 22000) //Earliest Windows 11 build
                         return OperatingSystemVersion.WINDOWS_11;
                     else if (buildNumber >= 10240) //Earliest Windows 10 build
                         return OperatingSystemVersion.WINDOWS_10;
                     else
-                        throw new UnknownOperatingSystemException("The operating system is an unknown build number.");
+                        throw new UnsupportedOperatingSystemException("The operating system is an unknown build number.");
                 })
                 .orElseThrow(() -> new CommandRunnerException("Unable to determine Windows version from command output."));
     }
 
+    /**
+     * Checks if the current OS is Linux using {@link Platform#isLinux()}.
+     *
+     * @return true if the OS is Linux; false otherwise.
+     */
     private static boolean isLinux() {
         return Platform.isLinux();
     }
